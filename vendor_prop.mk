3de84ac622a35605e276ce24a7bdac3b00d58a5a
Alexis <alexisr245@gmail.com>
2019-01-20 15:42:53 +0100

    feat: define ro.build.target_country

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -2,6 +2,10 @@
 # Vendor Properties for International LG G6
 #
 
+# Global
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.target_country=EU
+
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     telephony.lteOnGsmDevice=1
